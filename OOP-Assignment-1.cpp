1c40e73ff1f3139ebdd7af3347dae84f627a2631
Abdullah Awan <mlkabawan336@gmail.com>
2022-03-13 02:19:19 +0500

    Assignment

--- OOP-Assignment-1.cpp
+++ OOP-Assignment-1.cpp
@@ -3,32 +3,35 @@ using namespace std;
 
 int main()
 {
-    int month = 12, days = 30, hrs = 24;
+    int months = 2, days = 3, hrs = 4, act = 2;
     int count = 0;
 
-    int*** a = new int** [month];
+    int*** a = new int** [months];
 
-    for (int i = 0; i < month; i++) {
+    for (int i = 0; i < months; i++) {
         a[i] = new int* [days];
         for (int j = 0; j < days; j++) {
-            a[i][j] = new int[hrs];
+            a[i][j] = new int [hrs];
         }
     }
 
-    for (int i = 0; i < month; i++) {
+    for (int i = 0; i < months; i++) {
+        **a[i] = i;
         for (int j = 0; j < days; j++) {
+            *a[i][j] = j;
             for (int k = 0; k < hrs; k++) {
-                a[i][j][k] = ++count;
+                a[i][j][k] = k + 1;
             }
         }
     }
 
-    for (int i = 0; i < month; i++) {
+    for (int i = 0; i < months; i++) {
+        cout << "Month: " << **a[i] << endl;
         for (int j = 0; j < days; j++) {
+            cout << " -> Day: " << *a[i][j] << endl;
             for (int k = 0; k < hrs; k++) {
-                cout << a[i][j][k] << " ";
+                cout << " ->  -> Hour: " << a[i][j][k] << endl;
             }
-            cout << endl;
         }
         cout << endl;
     }
