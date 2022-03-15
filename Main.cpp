75812f21984dbc0030020f08f1c1fe2045e8d0bb
Abdullah Awan <mlkabawan336@gmail.com>
2022-03-15 05:02:04 +0500

    split

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,8 @@
 #include<stdio.h>
 #include <windows.h>
 #include <cwchar>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -24,6 +26,46 @@ void setCursorPointer(int x = 0, int y = 0)
 	SetConsoleCursorPosition(handle, coordinates);
 }
 
+void split(string act) {
+
+	string day, month , start_time , end_time;
+	int space = 0;
+	int n = 0;
+	int count = 0;
+	for (int i = 0; i < act.size(); i++) {
+		{
+			if (act[i] != '/') {
+				day += act[i];
+			}
+			else if (act[i] == '/') {
+				n = i + 1;
+				break;
+			}
+		}
+	}
+	for (int i = n; i < act.size(); i++) {
+		if (act[i] != ',' && count == 0) {
+				month += act[i];
+		}
+		else if (act[i] == ',') {
+			count++;
+		}
+		if (act[i] != ',' && count == 1) {
+			start_time += act[i];
+		}
+		else if (act[i] == ','){
+			count++;
+		}
+		if (act[i] != ',' && count == 2) {
+			end_time += act[i];
+		}
+		else if (act[i] == ',') {
+			count++;
+		}
+	}
+	cout << day << " " << month << " " << start_time << " " << end_time;
+}
+
 void Print_Calender(Activity**** Calender)
 {
 	system("cls");
@@ -31,36 +73,39 @@ void Print_Calender(Activity**** Calender)
 	int x = 0;
 	int y = 0;
 	int Days = 0;
-	setCursorPointer(5, 0);
-	cout << "January" << endl;
-	setCursorPointer(50, 0);
-	cout << "Febraury" << endl;
-	setCursorPointer(90, 0);
-	cout << "March" << endl;
-	setCursorPointer(5, 10);
-	cout << "April" << endl;
-	setCursorPointer(50, 10);
-	cout << "May" << endl;
-	setCursorPointer(90, 10);
-	cout << "June" << endl;
-	setCursorPointer(5, 20);
-	cout << "July" << endl;
-	setCursorPointer(50, 20);
-	cout << "August" << endl;
-	setCursorPointer(90, 20);
-	cout << "September" << endl;
-	setCursorPointer(5, 30);
-	cout << "October" << endl;
-	setCursorPointer(50, 30);
-	cout << "November" << endl;
-	setCursorPointer(90, 30);
-	cout << "December" << endl;
+	setCursorPointer(2, 0);
+	cout << "---January---" << endl;
+	setCursorPointer(27, 0);
+	cout << "---Febraury---" << endl;
+	setCursorPointer(52, 0);
+	cout << "---March---" << endl;
+	setCursorPointer(2, 10);
+	cout << "---April---" << endl;
+	setCursorPointer(30, 10);
+	cout << "---May---" << endl;
+	setCursorPointer(52, 10);
+	cout << "---June---" << endl;
+	setCursorPointer(2, 20);
+	cout << "---July---" << endl;
+	setCursorPointer(28, 20);
+	cout << "---August---" << endl;
+	setCursorPointer(51, 20);
+	cout << "---September---" << endl;
+	setCursorPointer(2, 30);
+	cout << "---October---" << endl;
+	setCursorPointer(27, 30);
+	cout << "---November---" << endl;
+	setCursorPointer(51, 30);
+	cout << "---December---" << endl;
+
 	int cx = 0, px = 0, py = 0, count = 0;
+	bool flag = true;
+
 	for (int i = 0; i < 12; i++)
 	{
 		if (i == 0)
 		{
-			//Days = 31;
+			Days = 31;
 			x = 9;
 			y = 2;
 			cx = 0;
@@ -69,25 +114,25 @@ void Print_Calender(Activity**** Calender)
 		}
 		else if (i == 1)
 		{
-			//Days = 28;
+			Days = 28;
 			x = 42;
 			y = 2;
-			cx = 45;
+			cx = 24;
 			px = cx;
 			py = y - 1;
 		}
 		else if (i == 2)
 		{
-			//Days = 31;
-			x = 78;
+			Days = 31;
+			x = 66;
 			y = 2;
-			cx = 84;
+			cx = 48;
 			px = cx;
 			py = y - 1;
 		}
 		else if (i == 3)
 		{
-			//Days = 30;
+			Days = 30;
 			x = 6;
 			y = 12;
 			cx = 0;
@@ -96,25 +141,25 @@ void Print_Calender(Activity**** Calender)
 		}
 		else if (i == 4)
 		{
-			//Days = 31;
+			Days = 31;
 			x = 36;
 			y = 12;
-			cx = 45;
+			cx = 24;
 			px = cx;
 			py = y - 1;
 		}
 		else if (i == 5)
 		{
-			//Days = 30;
-			x = 84;
+			Days = 30;
+			x = 48;
 			y = 12;
-			cx = 84;
+			cx = 48;
 			px = cx;
 			py = y - 1;
 		}
 		else if (i == 6)
 		{
-			//Days = 31;
+			Days = 31;
 			x = 6;
 			y = 22;
 			cx = 0;
@@ -123,25 +168,25 @@ void Print_Calender(Activity**** Calender)
 		}
 		else if (i == 7)
 		{
-			//Days = 31;
+			Days = 31;
 			x = 39;
 			y = 22;
-			cx = 45;
+			cx = 24;
 			px = cx;
 			py = y - 1;
 		}
 		else if (i == 8)
 		{
-			//Days = 30;
+			Days = 30;
 			x = 51;
 			y = 22;
-			cx = 84;
+			cx = 48;
 			px = cx;
 			py = y - 1;
 		}
 		else if (i == 9)
 		{
-			//Days = 31;
+			Days = 31;
 			x = 9;
 			y = 32;
 			cx = 0;
@@ -150,21 +195,33 @@ void Print_Calender(Activity**** Calender)
 		}
 		else if (i == 10)
 		{
-			//Days = 30;
+			Days = 30;
 			x = 42;
 			y = 32;
-			cx = 45;
+			cx = 24;
 			px = cx;
 			py = y - 1;
 		}
 		else if (i == 11)
 		{
-			//Days = 31;
+			Days = 31;
 			x = 51;
 			y = 32;
-			cx = 84;
+			cx = 48;
 			px = cx;
 			py = y - 1;
+		}
+		for (int j = 1; j <= Days; j++)
+		{
+			setCursorPointer(x, y);
+			cout << j;
+			x = x + 3;
+			if (x == 21 || x == 45 || x == 69)
+			{
+				x = cx;
+				y++;
+			}
+
 		}
 
 		setCursorPointer(px, py);
@@ -191,24 +248,34 @@ void Print_Calender(Activity**** Calender)
 	}
 }
 
+
 int main()
 {
 	ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
 
-	CONSOLE_FONT_INFOEX cfi;
-	cfi.cbSize = sizeof(cfi);
-	cfi.nFont = 0;
-	cfi.dwFontSize.X = 0;                   // Width of each character in the font
-	cfi.dwFontSize.Y = 22;                  // Height
-	cfi.FontFamily = FF_DONTCARE;
-	cfi.FontWeight = FW_NORMAL;
-	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
-
 	int months = 12, days = 0, hours = 24;
-    int count = 0;
-    Activity**** Calender;
-    Calender = new Activity*** [months];
+	int count = 0;
+	Activity**** Calender;
+	Calender = new Activity * **[months];
 
+	string act;
+
+	fstream activity;
+	activity.open("Activity", ios::out);
+	if (!activity) {
+		cout << "File not created!" << endl;
+	}
+	else {
+		cout << "File created successfully!";
+		activity << "09/11,11,14,user1,act33,Gym,0.22";
+		activity.close();
+	}
+
+	ifstream Fetch("Activity");
+	while (getline(Fetch, act)) {
+		split(act);
+	}
+	activity.close();
 
 	for (int i = 0; i < months; i++)
 	{
@@ -224,7 +291,7 @@ int main()
 		{
 			days = 30;
 		}
-		Calender[i] = new Activity **[days];
+		Calender[i] = new Activity * *[days];
 		for (int j = 0; j < days; j++)
 		{
 			Calender[i][j] = new Activity * [hours];
@@ -238,42 +305,56 @@ int main()
 	while (1)
 	{
 		int choice;
-		cout << "\n\nWelcome to the Calendar...You Can use Any Function" << endl << endl;
-		cout << "1) Print the calendar." << endl;
+		cout << "\n\nWelcome to the Calendar.." << endl << endl;
+		cout << "1) List all activities of a given user during a time period." << endl;
+		cout << "2) List the 5 most important activities of a given user during a time period." << endl;
+		cout << "3) longest consecutive number of days in which the user has no activity. " << endl;
+		cout << "4) List all the clashing activities of a pair of users, during a time period." << endl;
+		cout << "5) list all hourly slots that are free for all these user in this time period." << endl;
+		cout << "6) Print activity stats for a given month" << endl;
+		cout << "7) Print Calendar Stats (for the whole year)" << endl;
+		cout << "8) Remove a user from the calendar" << endl;
+		cout << "9) Save the calendar" << endl;
+		cout << "10) Print the calendar." << endl << endl;
+		cout << "Please Choice (1 - 10): ";
 		cin >> choice;
 		cout << endl;
-		if(choice==1)
+		if (choice == 1)
 		{
+
+		}
+		else if (choice == 2)
+		{
+		}
+		else if (choice == 3)
+		{
+		}
+		else if (choice == 4)
+		{
+		}
+		else if (choice == 5)
+		{
+		}
+		else if (choice == 6)
+		{
+		}
+		else if (choice == 7)
+		{
+		}
+		else if (choice == 8)
+		{
+		}
+		else if (choice == 9)
+		{
+
+		}
+		else if (choice == 10) {
 			Print_Calender(Calender);
 		}
-		else if(choice==2)
+		else
 		{
-	    }
-	    else if(choice==3)
-	    {
-	    }
-	    else if(choice==4)
-	    {
-	    }
-	    else if(choice==5)
-	    {
-	    }
-	    else if(choice==6)
-	    {
-	    }
-	    else if(choice==7)
-	    {
-	    }
-	    else if(choice==8)
-	    {
-	    }
-	    else if(choice==9)
-	    {
-	    }
-	    else
-	    {
-	    	cout<<"Invalid Value"<<endl;
-	    }
-    }
+			cout << "Invalid Value.. Try Again" << endl;
+		}
+	}
 	return 0;
 }
